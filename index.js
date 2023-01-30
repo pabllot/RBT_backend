6b68fa58a3f8d90afb53e66f7b6bd46f83692dd1
Pabllot07 <pabllot07@outlook.com>
2023-01-30 20:09:56 -0300

    connected to mysql

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "rbt"
+})
+
+app.get("/", (req, res) => {
+    res.json('hello from the backend!!!')
+})
+
+app.get("/pizzas", (req, res) => {
+    const q = "SELECT * FROM pizzas"
+
+    db.query(q, (err,data)=>{
+        if(err) return res.json(err)
+
+        return res.json(data)
+    })
+})
 
 app.listen(8800, () => {
     console.log("Connected to backend!!")
