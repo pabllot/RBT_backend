058484854171a7ffafa7e947dc7c54b4d7e14455
Pabllot07 <pabllot07@outlook.com>
2023-01-31 19:17:27 -0300

    sending pizza to mysql

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ app.get("/pizzas", (req, res) => {
         return res.json(data)
     })
 })
+app.post("/pizzas", (req, res) => {
+    const q = "INSERT INTO `rbt`.`pedidos` (`name`, `pizza`, `price`, `quantity`) VALUES (?)"
+
+    const values = [req.body.form.name, req.body.form.pizza, req.body.form.price, req.body.form.quantity]
+
+    db.query(q, [values], (err,data)=>{
+        if(err) return res.json(err)
+
+        return res.json(data)
+    })
+})
 
 app.listen(8800, () => {
     console.log("Connected to backend!!")
