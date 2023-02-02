de4355e5404e8967b2ff90e6524f0788b7cf9214
Pabllot07 <pabllot07@outlook.com>
2023-02-02 11:34:26 -0300

    railway

--- connect.js
+++ connect.js
@@ -4,15 +4,15 @@ dotenv.config()
 
 export const db = mysql.createPool({
     connectionLimit : 100,
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST || 'localhost',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || 'password',
+    database: process.env.MYSQLDATABASE || 'rbt',
     connectionLimit: 100,
     queueLimit: 100,
     acquireTimeout: 1000000,
     connectTimeout: 30000,
     debug: false,
-    port: 3306
+    port: process.env.MYSQLPORT || 3306
 })
 
