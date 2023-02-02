e8a331cd51ae315ca8f11ed638fdeac23430c5e1
Pabllot07 <pabllot07@outlook.com>
2023-02-02 11:12:50 -0300

    test pool railway

--- connect.js
+++ connect.js
@@ -7,6 +7,11 @@ export const db = mysql.createPool({
     host: process.env.MYSQLHOST || 'localhost',
     user: process.env.MYSQLUSER || 'root',
     password: process.env.MYSQLPASSWORD || 'password',
-    database: process.env.MYSQLDATABASE || 'rbt'
+    database: process.env.MYSQLDATABASE || 'rbt',
+    connectionLimit: 100,
+    queueLimit: 100,
+    acquireTimeout: 1000000,
+    connectTimeout: 30000,
+    debug: false
 })
 
