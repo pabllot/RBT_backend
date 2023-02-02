63b2abda8225c79ed8f8b4bfa82f69af44c3257e
Pabllot07 <pabllot07@outlook.com>
2023-02-02 11:46:36 -0300

    createPool mysql

--- controllers.js
+++ controllers.js
@@ -2,75 +2,92 @@ import { db } from './connect.js'
 
 export const getOrders = (req, res) => {
     db.getConnection(function(err, connection) {
-        if (err) throw err; // not connected!
-      
-        // Use the connection
-        connection.query("SELECT * FROM pedidos", (err,data)=>{
+        if (err) throw err; 
+        const q = "SELECT * FROM pedidos"
+        connection.query(q, (err,data)=>{
             if(err) return res.json(err)
     
             return res.json(data)
         })
           connection.release();
-      
-          // Handle error after the release.
           if (err) throw err;
-      
-          // Don't use the connection here, it has been returned to the pool.
         });
-      }
+      } 
     
     
-    
-
-
-{/* export const getPizzas = (req, res) => {
+export const getPizzas = (req, res) => {
+    db.getConnection(function(err, connection) {
+        if (err) throw err; 
     const q = "SELECT * FROM pizzas"
 
-    db.query(q, (err,data)=>{
+    connection.query(q, (err,data)=>{
         if(err) return res.json(err)
 
         return res.json(data)
     })
-}
+      connection.release();
+      if (err) throw err;
+    });
+  } 
 
 export const addOrder = (req, res) => {
+    db.getConnection(function(err, connection) {
+        if (err) throw err; 
     const q = "INSERT INTO `rbt`.`pedidos` (`name`, `pizza`, `price`, `quantity`) VALUES (?)"
 
     const values = [req.body.form.name, req.body.form.pizza, req.body.form.price, req.body.form.quantity]
 
-    db.query(q, [values], (err,data)=>{
+    connection.query(q, [values], (err,data)=>{
         if(err) return res.json(err)
 
         return res.json(data)
     })
-}
+      connection.release();
+      if (err) throw err;
+    });
+  } 
 
 export const cancelOrder = (req, res) => {
+    db.getConnection(function(err, connection) {
+        if (err) throw err; 
     const q = "UPDATE `rbt`.`pedidos` SET `status` = '3' WHERE (`id` = ?)"
 
-    db.query(q, [req.body.id], (err,data)=>{
+    connection.query(q, [req.body.id], (err,data)=>{
         if(err) return res.json(err)
 
         return res.json(data)
     })
-}
+      connection.release();
+      if (err) throw err;
+    });
+  } 
 
 export const orderDone = (req, res) => {
+    db.getConnection(function(err, connection) {
+        if (err) throw err; 
     const q = "UPDATE `rbt`.`pedidos` SET `status` = '2' WHERE (`id` = ?)"
 
-    db.query(q, [req.body.id], (err,data)=>{
+    connection.query(q, [req.body.id], (err,data)=>{
         if(err) return res.json(err)
 
         return res.json(data)
     })
-}
+      connection.release();
+      if (err) throw err;
+    });
+  } 
 
 export const deleteOrder = (req, res) => {
+    db.getConnection(function(err, connection) {
+        if (err) throw err; 
     const q = "DELETE FROM `rbt`.`pedidos` WHERE (`id` = ?);"
 
-    db.query(q, [req.params.id], (err,data)=>{
+    connection.query(q, [req.params.id], (err,data)=>{
         if(err) return res.json(err)
 
         return res.json(data)
     })
-} */}
+      connection.release();
+      if (err) throw err;
+    });
+  } 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 
-import {getOrders } from './controllers.js'
+import { addOrder, cancelOrder, deleteOrder, getOrders, getPizzas, orderDone } from './controllers.js'
 
 const app = express()
 
@@ -15,11 +15,15 @@ app.get("/", (req, res) => {
     res.json('backend is working!!!' + PORT)
 })
 
+app.get("/pizzas", getPizzas)
 app.get("/pedidos", getOrders)
-
+app.post("/pizzas", addOrder)
+app.post("/pedidos/cancelados", cancelOrder)
+app.post("/pedidos/concluidos", orderDone)
+app.delete("/pedidos/delete/:id", deleteOrder)
 
 const PORT = process.env.PORT || 8800
 
-app.listen(`0.0.0.0:${PORT}` , () => {
+app.listen(PORT , () => {
     console.log("Connected to backend!" + PORT)
 })
